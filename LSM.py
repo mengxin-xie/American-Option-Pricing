821c650358b024a0879e58e3365c201d7aee049a
mengxin-xie <mengxin.xie1031@gmail.com>
2024-08-05 11:48:51 +0100

    Add files via upload

--- LSM.py
+++ LSM.py
@@ -1,5 +1,4 @@
 import numpy as np
-import pandas as pd
 from scipy.stats import norm
 from sklearn.linear_model import LinearRegression
 
@@ -15,6 +14,7 @@ def black_scholes_euro(S0,K,r,T, sigma, option_type):
     return price
 
 def simulate_gbm(M, N, T, S0, mu, sigma):
+    np.random.seed(1234)
     # calc each time step
     dt = T/N
     # simulation using numpy arrays
@@ -29,9 +29,7 @@ def simulate_gbm(M, N, T, S0, mu, sigma):
 
 
 # Least Square Monte Carlo
-
-
-def longstaff_schwartz(paths,strike,r, option_type):
+def longstaff_schwartz(paths, strike, r, option_type):
     # prices at T is recorded in the first row
     paths = paths[::-1]
     # initialize the cash flow matrix
@@ -40,15 +38,15 @@ def longstaff_schwartz(paths,strike,r, option_type):
         cash_flows[cash_flows<0] = 0
     else:
         cash_flows = strike - paths
-        cash_flows[cash_flows<0] = 0
+        cash_flows[cash_flows < 0] = 0
     # number of time steps
-    T = cash_flows.shape[0]-1
+    T = cash_flows.shape[0] - 1
     # number of path
     M = cash_flows.shape[1]
     for t in range(1,T):
         # Look at time T-t
         # Create index to only look at in the money paths at time T-t
-        in_the_money = paths[t,:]< strike
+        in_the_money = paths[t,:] < strike
         # Run Regression to obtain the conditional expectation function
         X = paths[t,in_the_money]
         X2 = X*X
@@ -61,17 +59,45 @@ def longstaff_schwartz(paths,strike,r, option_type):
         # continuation value
         conditional_exp = model.predict(Xs)
         continuations = np.zeros_like(paths[t,:])
-        continuations[in_the_money]= conditional_exp
+        continuations[in_the_money] = conditional_exp
         # compare the continuation value and excericse value
         ## notice that as the values for OTM and ATM paths in both continuations and cas
-        cash_flows[t,:] = np.where(continuations> cash_flows[t,:], 0, cash_flows[t,:])
+        cash_flows[t,:] = np.where(continuations > cash_flows[t,:], 0, cash_flows[t,:])
         # If early exercise is performed, subsequent cashflows = 0
         exercised_early = continuations < cash_flows[t, :]
         cash_flows[0:t,:][:,exercised_early] = 0
     # Return final option price
     discounted_cash_flows = np.zeros((T,M))
     for t in range(T):
-        discounted_cash_flows =
+        discounted_cash_flows[t] = cash_flows[t] * np.exp(-r * (T-t))
     option_price = discounted_cash_flows.sum(axis=0).sum()/M
 
     return option_price
+
+if __name__=="__main__":
+
+    # Parameters
+    # number of paths
+    M = 100000
+    # number of steps per year
+    N = 50
+    # time in years
+    T = 1
+    # initial stock price
+    S0 = 36
+    # volatility
+    sigma = 0.2
+    # strike
+    strike = 40
+    # annual risk-free interest rate
+    r = 0.06
+
+    BS_Price = black_scholes_euro(S0, strike, r, T, sigma, 'put')
+    print('BS_Price: ', BS_Price)
+
+    paths = simulate_gbm(M, N, T, S0, r, sigma)
+
+    LSM_Price = longstaff_schwartz(paths, strike, (1+r)**(1/N)-1, 'put')
+    print('LSM_Price: ', LSM_Price)
+
+
--- parallel.py
+++ parallel.py
@@ -1,120 +1,128 @@
 import numpy as np
-import pandas as pd
-from scipy.stats import norm
 from sklearn.linear_model import LinearRegression
-import matplotlib.pyplot as plt
-
 
 def simulate_gbm(M, N, T, S0, mu, sigma):
-    # calc each time step
+    np.random.seed(1234)
     dt = T/N
-    # simulation using numpy arrays
     dW = np.random.normal(0, np.sqrt(dt),size=(M,N)).T
     St = np.exp((mu-sigma ** 2 / 2) * dt + sigma * dW)
-    # include array of 1's
     St = np.vstack([np.ones(M),St])
     St[0] = S0
-    # multiply through by s0 and return the cumulative product of elements along a given simulation
     St = St.cumprod(axis=0)
     return St
 
-# Parameters
-# number of excersie dates
-M = 4
-# total number of paths
-N = 30
-# number of iterations
-n = 10
-# time in years
-T = 1
-# initial stock price
-S0 = 36
-# volatility
-sigma = 0.2
-# strike
-strike = 40
-# let annual risk free interest rate be 0.06
-# option price
-Option_price = 0
-# discounted price for all iterations - a list
-Price = [0]*n
-# price info from iteration 1 to current iteration - a dictionary with k as the key
-X = dict()
-Y = dict()
-# model info for each time step - a dictionary
-models = dict()
+def Parallel(M, N, n, T, S0, sigma, strike, r):
 
+    # discounted price for all iterations - a list
+    Price = [0]*n
+    # price info from iteration 1 to current iteration - a dictionary with k as the key
+    X = dict()
+    Y = dict()
+    # model info for each time step - a dictionary
+    models = dict()
 
-# initializaiton:
-paths =simulate_gbm(int(N/n), M, T, S, 0.06, sigma)
-euro_discounted_prices = np.ones_like(paths) * np.exp(-0.06)
-final_payoff = strike - paths[-1]
-euro_discounted_prices[0] = np.where(final_payoff > 0, final_payoff, 0)
-euro_discounted_prices = euro_discounted_prices.cumprod(axis=0)
-euro_discounted_prices = euro_discounted_prices[::-1]
-euro_discounted_prices
-
-for k in range(1,M+1):
-    # update x[k] by including the current paths info
-    x= paths[k]
-    x2 = x*x
-    xs = np.column_stack([x,x2])
-    X[k] = xs
-    if k==M:
-        Y[k] = euro_discounted_prices[k]
-    else:
-        Y[k]= euro_discounted_prices[k+1] * np.exp(-0.06)
+    # initializaiton:
+    paths = simulate_gbm(int(N/n), M, T, S0, r, sigma)
+    euro_discounted_prices = np.ones_like(paths) * np.exp(-r/M)
+    final_payoff = strike - paths[-1]
+    euro_discounted_prices[0] = np.where(final_payoff > 0, final_payoff, 0)
+    euro_discounted_prices = euro_discounted_prices.cumprod(axis=0)
+    euro_discounted_prices = euro_discounted_prices[::-1]
+    for k in range(1,M+1):
+        x = paths[k]
+        x2 = x*x
+        x3 = k*np.ones_like(x)
+        x4 = k*x
+        x5 = k*x2
+        xs = np.column_stack([x,x2,x3,x4,x5])
+        X[k] = xs
+        if k==M:
+            Y[k] = euro_discounted_prices[k]
+        else:
+            Y[k]= euro_discounted_prices[k+1] * np.exp(-r/M)
         model_sklearn = LinearRegression()
-        model = model_sklearn.fit(x[k], Y[k])
-    models[k] = model
+        model = model_sklearn.fit(X[k], Y[k])
+        models[k] = model
+
+    # iterations
+    for i in range(1,n+1):
+        # for one interation
+        # step 1:paths simulation
+        paths =simulate_gbm(int(N/n), M, T, S0, r, sigma)
+
+        # step 2.1: payoff value calculation
+        payoff_values = strike - paths
+        payoff_values[payoff_values<0] = 0
+        # step 2.2: continuation value estimate:
+        continuation_values = np.zeros_like(paths)
+        for k in range(1,M+1):
+            # in each time step, calculate the continuation values for different paths
+            # using corresponding regression model(alpha_k)
+            x = paths[k]
+            x2 = x*x
+            x3 = k * np.ones_like(x)
+            x4 = k * x
+            x5 = k * x2
+            xs = np.column_stack([x, x2, x3, x4, x5])
+            local_model = models[k]
+            continuation_values[k]= local_model.predict(xs)
+
+        # step 3: comparison between continuation value and exrecise value
+        discounted_price = payoff_values
+        for k in range(M-1,0,-1):
+            no_exercise = discounted_price[k] < continuation_values[k]
+            discounted_price[k][no_exercise]= discounted_price[k+1][no_exercise] * np.exp(-r/M)
+
+        # step 4: sum of t=1 prices
+        Price[i-1]= np.exp(-r/M) * discounted_price[1].sum()
+
+        # step 5:update the regression models
+        for k in range(1,M+1):
+            x = paths[k]
+            x2 = x * x
+            x3 = k * np.ones_like(x)
+            x4 = k * x
+            x5 = k * x2
+            xs = np.column_stack([x, x2, x3, x4, x5])
+            X[k]= np.vstack([X[k],xs])
+            if k==M:
+                Y[k] = np.hstack([Y[k],discounted_price[k]])
+            else:
+                Y[k] = np.hstack([Y[k],discounted_price[k+1] * np.exp(-r/M)])
+            model_sklearn = LinearRegression()
+            model = model_sklearn.fit(X[k], Y[k])
+            models[k] = model
+
+    # weights given to different iterations
+    W = np.array([0.99*(i-1) for i in range(1,n+1)])
+    W = np.tanh(W)
+    W = 1 - 0.5 * (1 - W)
+    Price = np.array(Price) / (N/n)
+    Option_price = sum(Price * W) / W.sum()
+
+    return Option_price
 
 
-
-# for one interation
-i=1
-# step 1:paths simulation
-# stimulate paths in this set
-paths =simulate_gbm(int(N/n),M,T,S0,0.06, sigma)
-#3 paths and 4 time steps
-paths
-
-# step 2.1:payoff value calculation
-# assume put option with strike
-payoff_values = strike - paths
-payoff_values[payoff_values<0] = 0
-payoff_values
-# step 2.2:continuation value estimate:
-# assume a simple continuation value function of y ~ x + x^2 first
-continuation_values = np.zeros_like(paths)
-for k in range(1,M+1):
-    # in each time step, calculate the continuation values for different path
-    # using corresponding regression model(alpha_k)
-    x = paths[k]
-    x2 = x*x
-    xs = np.column_stack([x,x2])
-    local_model = models[k]
-    continuation_values[k]= local_model.predict(xs)
+if __name__=="__main__":
+    # Parameters
+    # number of excersie dates per year
+    M = 50
+    # total number of paths
+    N = 100000
+    # number of iterations
+    n = 10
+    # time in years
+    T = 1
+    # initial stock price
+    S0 = 36
+    # volatility
+    sigma = 0.2
+    # strike
+    strike = 40
+    # let annual risk-free interest rate
+    r = 0.06
 
 
-#step 3:comparison between continuation value and exrecise value
-discounted_price = payoff_values
+    Parallel_Price = Parallel(M, N, n, T, S0, sigma, strike, r)
+    print('Parallel_Price: ', Parallel_Price)
 
-for k in range(M-1,0,-1):
-    no_exercise = discounted_price[k]<continuation_values[k]
-    discounted_price[k][no_exercise]= discounted_price[k+1][no_exercise] * np.exp(-0.06)
-discounted_price
-# step 4:sum of t=1 prices
-Price[i-1]= np.exp(-0.06) * discounted_price[11].sum()
-# step 5:update the regression models
-for k in range(1,M+1):
-    x = paths[k]
-    x2 = x*x
-    xs = np.column_stack([x,x2])
-    x[k]= np.vstack([X[k],xs])
-    if k==M:
-        Y[k]= np.hstack([Y[k],discounted_price[k]])
-    else:
-        Y[k]= np.hstack([Y[k],discounted_price[k+1] * np.exp(-0.06)])
-    model_sklearn = LinearRegression()
-    mode1 = model_sklearn.fit(X[k], Y[k])
-    models[k] = model
